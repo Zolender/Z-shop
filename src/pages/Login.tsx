6b5cb2dc4e38033e2dff536d318997fd02c5f6c8
Zolender <ndeingare@gmail.com>
2026-04-05 22:46:42 +0200

    refactor: improve Login component layout and styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,25 @@
 const Login = () => {
     return (
-        <>
-        <form className="bg-blue-100 p-12 rounded-lg shadow-md">
-            <h1 className="text-3xl font-bold text-blue-500">Login</h1>
-            <div className="mt-8">
-                <label className="block text-blue-500 text-lg font-bold mb-2" htmlFor="name">Name</label>
-                <input type="text" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 text-blue-700" />
+        <div className="flex items-center justify-center">
+            <div className="bg-white p-12 rounded-lg shadow-md max-w-md">
+                <h1 className="text-3xl font-bold text-blue-500">Login</h1>
+                <form className="mt-8">
+                    <div className="mb-6">
+                        <label className="block text-blue-500 text-lg font-bold mb-2" htmlFor="name">Name</label>
+                        <input type="text" id="name" className="border border-blue-300 rounded w-full py-2 px-3 text-blue-700" />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-blue-500 text-lg font-bold mb-2" htmlFor="email">Email</label>
+                        <input type="email" id="email" className="border border-blue-300 rounded w-full py-2 px-3 text-blue-700" />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
+                    </div>
+                </form>
             </div>
-            <div className="mt-8">
-                <label className="block text-blue-500 text-lg font-bold mb-2" htmlFor="email">Email</label>
-                <input type="email" id="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-blue-700" />
-            </div>
-            <div className="mt-8">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
-            </div>
-        </form>
-        </>
+        </div>
     );
 }
 
-export default Login
+export default Login;
+
