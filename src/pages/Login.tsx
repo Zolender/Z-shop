00359253a94cdf9e7c78a5b03902d7cb0349e728
Zolender <ndeingare@gmail.com>
2026-04-06 15:11:44 +0200

    feat: enhance Login component with form handling for email and password inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import useFetch from "../hooks/useFetch";
+
 const Login = () => {
+
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
+
+
     return (
-        <>
+        <div className="">
+
+            <form onSubmit={e=>handleSubmit(e)}>
+                <label htmlFor="email">Email: </label>
+                <input type="email" required value={email} onChange={(e)=>setEmail(e.target.value)} /> <br />
+                <label htmlFor="paswword">Password:</label>
+                <input type="password" required value={password} onChange={(e)=>setPassword(e.target.value)} /> <br />
+                <input type="submit" value="Login" />
+            </form>
         
-        </>
+        </div>
     );
 }
 
 export default Login;
+
 
