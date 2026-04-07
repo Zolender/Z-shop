7eea474e6ab2e5cb5cb4bfe1a4aec53cf2772cf9
Zolender <ndeingare@gmail.com>
2026-04-07 20:05:02 +0200

    feat: add ThemeContext and ThemeProvider for theme management

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,39 @@
+import { useContext, useState } from "react";
+import { createContext } from "react";
+
+type Theme = "light" | "dark"
+
+type ThemeContextType = {
+    theme: Theme
+    toggleTheme: ()=>void
+}
+
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export function ThemeProvider({children}: {children: React.ReactNode}){
+    const [theme, setTheme] = useState<Theme>(()=>{
+        const stored = localStorage.getItem("theme")
+        return stored? stored as Theme: "light" as Theme
+    })
+
+    function toggleTheme(){
+        setTheme(prev=>{
+            const newTheme= prev==="dark"? "light": "dark"
+            localStorage.setItem("theme", newTheme) 
+            return newTheme
+        })
+    }
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export function useTheme(){
+    const theme = useContext(ThemeContext)
+    if(!theme)throw new Error("The theme must be used inside the provider's components")
+    return theme
+}
