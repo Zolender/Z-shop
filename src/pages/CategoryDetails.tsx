b77359278e5cb321a11c879ae3f217d4d55b1b72
Zolender <ndeingare@gmail.com>
2026-04-07 14:30:14 +0200

    feat: add Product type definition to CategoryDetails component

--- src/pages/CategoryDetails.tsx
+++ src/pages/CategoryDetails.tsx
@@ -1,3 +1,23 @@
+
+type Product = {
+    id: number
+    title: string
+    slug: string
+    price: number
+    description: string
+    images: string[]
+    category: {
+        id: number
+        name: string
+        slug: string
+        image: string
+        creationAt: string
+        updatedAt: string
+    }
+    creationAt: string
+    updated: string
+}
+
 const CategoryDetails = () => {
     return (
         <></>
