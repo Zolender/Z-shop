045aaa46c7afc04302a1ad6b66af297aa8bb5a71
Zolender <ndeingare@gmail.com>
2026-04-07 15:05:35 +0200

    feat: add NavBar component to Layout for improved navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./Navbar";
 
 
 export default function Layout(){
     return (
         <div className="">
+            <NavBar/>
             <main>
                 <Outlet/>
             </main>
