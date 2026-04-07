eda96478dc938c845c9fe4296e37b6cd62a0f8d5
Zolender <ndeingare@gmail.com>
2026-04-07 15:00:14 +0200

    feat: add Layout component for main application structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+
+export default function Layout(){
+    return (
+        <div className="">
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    )
+}
