4785cb95719ae427e061dd028d9b4ea8574ce8df
Zolender <ndeingare@gmail.com>
2026-04-07 14:47:14 +0200

    feat: implement CategoryDetails component with product fetching and display

--- src/pages/CategoryDetails.tsx
+++ src/pages/CategoryDetails.tsx
@@ -1,3 +1,5 @@
+import { useNavigate, useParams } from "react-router-dom"
+import useFetch from "../hooks/useFetch"
 
 type Product = {
     id: number
@@ -19,8 +21,29 @@ type Product = {
 }
 
 const CategoryDetails = () => {
+    const {id} = useParams()
+    const navigate = useNavigate();
+
+    const {data: products, isLoading, error} = useFetch<Product[]>(`https://api.escuelajs.co/api/v1/products/?categoryId=${id}`)
+
+    if (isLoading)return <p className="">Loading products...</p>
+    if (error)return <p className="">Error: {error.message}</p>
+
     return (
-        <></>
+        <div className="">
+            <h1 className="">{products?.[0]?.category.name?? "Category"}</h1>
+            <div className="">
+                {products?.length === 0 && (<p className="">No products found in this category.</p>)}
+
+                {products?.map(product=>(
+                    <div className="hover:cursor-pointer" key={product.id} onClick={()=> navigate(`/products/${product.id}`)}>
+                        <img src={product.images[0]} alt={product.title} />
+                        <h3 className="">{product.title}</h3>
+                        <p className="">{product.price}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
  
