0571d1fd9911f47e89844d3e4e80e819f204fcca
Zolender <ndeingare@gmail.com>
2026-04-08 16:02:14 +0200

    feat: enhance Categories component with improved layout and styling

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -20,13 +20,13 @@ const Categories = () => {
     if(error)return <p className="">Error: {error.message}</p>
     
     return (
-        <div className="">
-            <h1 className="">Categories</h1>
-            <div>
+        <div className="flex flex-col  justify-between p-5 ">
+            <h1 className="text-center text-4xl font-mono font-bold text-cyan-800 mb-10">Categories</h1>
+            <div className="grid grid-cols-4 gap-5">
                 {categories?.map((category)=>(
-                    <div key={category.id} onClick={()=>navigate(`/categories/${category.id}`)} className="hover:cursor-pointer">
-                        <img src={category.image} alt={category.name} />
-                        <h3 className="">{category.name}</h3>
+                    <div key={category.id} onClick={()=>navigate(`/categories/${category.id}`)} className=" p-2 flex flex-col items-center border transition-all duration-300 ease-in-out hover:-translate-y-1 hover:cursor-pointer justify-between">
+                        <img className="h-5/6 rounded-md" src={category.image} alt={category.name} />
+                        <h3 className="text-lg font-mono text-cyan-600">{category.name}</h3>
                     </div>
                 ))}
             </div>
