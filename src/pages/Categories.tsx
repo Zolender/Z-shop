b7c8b303ab87b02017bb6ba4a81b3ea8e9e3aa1f
Zolender <ndeingare@gmail.com>
2026-04-10 13:34:50 +0200

    feat: enhance loading and error messages in Categories and CategoryProducts components; improve layout in Layout component

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -16,17 +16,17 @@ const Categories = () => {
 
     const {data: categories, isLoading, error} = useFetch<Category[]>("https://api.escuelajs.co/api/v1/categories")
 
-    if(isLoading)return <p className="">Loading categories...</p>
-    if(error)return <p className="">Error: {error.message}</p>
+    if(isLoading)return <p className="flex items-center justify-center font-bold text-4xl font-mono animate-pulse h-screen">Loading categories...</p>
+    if(error)return <p className="flex items-center justify-center font-bold text-2xl font-mono h-screen text-red-400">Error: {error.message}</p>
     
-    return (
+    if(!isLoading && !error)return (
         <div className="flex flex-col  justify-between p-5 ">
             <h1 className="text-center text-4xl font-mono font-bold text-cyan-800 mb-10">Categories</h1>
             <div className="grid grid-cols-4 gap-5">
                 {categories?.map((category)=>(
-                    <div key={category.id} onClick={()=>navigate(`/categories/${category.id}`)} className=" p-2 flex flex-col items-center border transition-all duration-300 ease-in-out hover:-translate-y-1 hover:cursor-pointer justify-between">
+                    <div key={category.id} onClick={()=>navigate(`/categories/${category.id}`)} className=" p-2 flex flex-col overflow-hidden items-center rounded-md  bg-[#00eeff9a] transition-all duration-300 ease-in-out hover:scale-105 hover:cursor-pointer justify-between">
                         <img className="h-5/6 rounded-md" src={category.image} alt={category.name} />
-                        <h3 className="text-lg font-mono text-cyan-600">{category.name}</h3>
+                        <h3 className="text-2xl font-mono text-slate-600 font-bold ">{category.name}</h3>
                     </div>
                 ))}
             </div>
--- src/pages/CategoryProducts.tsx
+++ src/pages/CategoryProducts.tsx
@@ -26,17 +26,18 @@ const CategoryProducts = () => {
 
     const {data: products, isLoading, error} = useFetch<Product[]>(`https://api.escuelajs.co/api/v1/products/?categoryId=${id}`)
 
-    if (isLoading)return <p className="">Loading products...</p>
-    if (error)return <p className="">Error: {error.message}</p>
+    if (isLoading)return <p className="flex items-center justify-center font-bold text-4xl font-mono animate-pulse h-screen">Loading products...</p>
+    if (error)return <p className="flex items-center justify-center font-bold text-4xl font-mono  h-screen">Error: {error.message}</p>
 
     return (
         <div className="px-5">
+            <button className="px-5 py-2 bg-blue-400 hover:cursor-pointer hover:bg-blue-500 transition-colors duration-300 ease-in font-bold text-lg rounded-md text-slate-200" onClick={()=> navigate(-1)}>← Back</button>
             <h1 className="text-center text-4xl font-mono font-bold text-cyan-800 mb-10">{products?.[0]?.category.name?? "Category"}</h1>
             <div className="grid grid-cols-4 gap-5">
                 {products?.length === 0 && (<p className="text-center text-6xl font-mono font-bold text-red-800">No products found in this category.</p>)}
 
                 {products?.map(product=>(
-                    <div className="p-2 rounded-md hover:shadow-md hover:shadow-black flex flex-col items-center border transition-all duration-300 ease-in-out hover:-translate-y-1 hover:cursor-pointer justify-between" key={product.id} onClick={()=> navigate(`/products/${product.id}`)}>
+                    <div className=" p-2 flex flex-col overflow-hidden items-center rounded-md  bg-[#00eeff9a] transition-all duration-300 ease-in-out hover:scale-105 hover:cursor-pointer justify-between" key={product.id} onClick={()=> navigate(`/products/${product.id}`)}>
                         <img className="h-5/6 rounded-md" src={product.images[0]} alt={product.title} />
                         <h3 className="text-lg font-mono text-cyan-600">{product.title}</h3>
                         <p className="text-md font-mono text-cyan-500">{product.price}</p>
